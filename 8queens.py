d6fd4eee671ac4cd63c26cb1249cccb49967abb7
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2016-01-05 16:57:01 -0500

    added matasano crypto challenge file
    started working on matasano crypto challenges from cryptopals.com

--- 8queens.py
+++ 8queens.py
@@ -4,30 +4,25 @@ Created on Wed Jun 10 18:07:01 2015
 8 queens solver
 @author: Matthew Vander Hoff
 """
+import random
 
 
+n = 8
+solutions = []
+current = []
 
-board = [[False] * 8 ] * 8
+def place(col):
+    possible = []
+    attacks = []
+    for x in current:
+        attacks.append(x+col)
+        attacks.append(x-col)
+    for x in range(n):
+        if x not in current:
+            if x not in attacks:
+                possible.append(x)
+    return possible
 
-def check(x, y):
-    # check row
-    for x1 in range(8):
-        if board[y][x1] == True : return False
-    # check col
-    for y1 in range(8):
-        if board[y1][x] == True : return False
-    # check diags
-    #for daig in range(8):
-        #if board[][] == 1 : return False:
-    return True
-
-def drawDiag(x,y):
-    diags = []
-    for u in range(8):
-        if (x+u <= 8 and y-u >= 0):
-            diags.add((x+u, y-u))
-        if (x-u >= 0 and y-u >= 0):
-            diags.add(x-u, y-u)
-
-def place(x,y):
-    pass
+def start():
+    for x in range(n):
+        place(x)
---
+++ matasano.py
@@ -0,0 +1,69 @@
+import string
+import base64
+import pprint as p
+
+english = {"E":12.02,
+            "T":9.10,
+            "A":8.12,
+            "O":7.68,
+            "I":7.31,
+            "N":6.95,
+            "S":6.28,
+            "R":6.02,
+            "H":5.92,
+            "D":4.32,
+            "L":3.98,
+            "U":2.88,
+            "C":2.71,
+            "M":2.61,
+            "F":2.30,
+            "Y":2.11,
+            "W":2.09,
+            "G":2.03,
+            "P":1.82,
+            "B":1.49,
+            "V":1.11,
+            "K":0.69,
+            "X":0.17,
+            "Q":0.11,
+            "J":0.10,
+            "Z":0.07}
+
+def challenge1():
+    chalstring = bytearray([0x49,0x27,0x6d,0x20,0x6b,0x69,0x6c,0x6c,0x69,0x6e,0x67,0x20,0x79,0x6f,0x75,0x72,0x20,0x62,0x72,0x61,0x69,0x6e,0x20,0x6c,0x69,0x6b,0x65,0x20,0x61,0x20,0x70,0x6f,0x69,0x73,0x6f,0x6e,0x6f,0x75,0x73,0x20,0x6d,0x75,0x73,0x68,0x72,0x6f,0x6f,0x6d])
+    print(base64.b64encode(chalstring))
+
+def challenge2():
+    chalstring = "1c0111001f010100061a024b53535009181c"
+    xorstring = "686974207468652062756c6c277320657965"
+    chalhex = bytearray.fromhex(chalstring)
+    xorhex = bytearray.fromhex(xorstring)
+    output = bytearray()
+    for x in range(len(chalhex)):
+        output.append(chalhex[x] ^ xorhex[x])
+    return output
+
+def challenge3():
+    chalstring = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    chalhex = bytearray.fromhex(chalstring)
+    f = get_freq(chalhex)
+    print_freqs(f)
+    # found that space is normally the most apperent charicter!
+
+
+def get_freq(barr):
+    length = len(barr)
+    freqs = {}
+    for x in range(length):
+        freqs[barr[x]] = freqs.setdefault(barr[x], 0) + 1
+    return freqs
+
+def print_freqs(f):
+    for x in f:
+        print("freq of {} is {:.2%}".format(x, int(f[x])/len(f)))
+
+
+
+challenge1()
+print(repr(challenge2()))
+challenge3()
